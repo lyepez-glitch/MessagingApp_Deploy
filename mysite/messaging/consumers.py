d613d8fcc64bf06f9a884e47af4255ef0f078788
Lucas Yepez <lucasyepez99@gmail.com>
2024-09-20 10:54:12 -0500

    fixed undefine dbug

--- mysite/messaging/consumers.py
+++ mysite/messaging/consumers.py
@@ -1,58 +1,40 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
 import json
 
 class MessageConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print(self.scope)
+        self.group_name = 'chat_group'  # Use your own group name
 
-        self.group_name = 'chat_group'
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
+        # Join group
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
+        # Leave group
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data):
-        from .models import Message  # Import here to avoid AppRegistryNotReady error
-
         data = json.loads(text_data)
-        message_content = data['content']
-        receiver_id = data.get('receiver_id')  # Get the receiver ID if it's provided
+        content = data['content']
+        receiver_id = data['receiver_id']
+        sender_username = self.scope['user'].username  # Get the username from the request scope
 
-        # Get the actual User instance from the scope
-        user = self.scope['user']
-
-        if user.is_authenticated:
-            # Save the message to your database
-            await database_sync_to_async(Message.objects.create)(
-                content=message_content,
-                sender=user,  # Use the User instance
-                receiver_id=receiver_id
-            )
-
-            # Broadcast the message to the group
-            await self.channel_layer.group_send(
-                self.group_name,
-                {
-                    'type': 'chat_message',
-                    'content': message_content
-                }
-            )
-        else:
-            # Handle unauthenticated user case if necessary
-            await self.send(text_data=json.dumps({
-                'error': 'User not authenticated'
-            }))
+        # Send the message to the group
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'chat_message',
+                'content': content,
+                'sender_username': sender_username,  # Include sender's username
+            }
+        )
 
     async def chat_message(self, event):
-        message = event['content']
+        content = event['content']
+        sender_username = event['sender_username']
+
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'content': message
+            'content': content,
+            'sender_username': sender_username,  # Send sender's username back
         }))
