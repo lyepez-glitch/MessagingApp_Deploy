7fbaece5e7b55cf44c93d957fda261507dabd17c
Lucas Yepez <lucasyepez99@gmail.com>
2024-10-26 13:24:00 -0500

    prep for deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4=s-y6ua&8kha!8ucdmr1yj6d*t%8n9y+-nyxanry#nmmh9573
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['messagingApp.onrender.com']
+ALLOWED_HOSTS = ['messagingapp-vvlg.onrender.com']
 
 
 # Application definition
