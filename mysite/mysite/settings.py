293fd978414b9f94bde3daad405176e8bdb873ef
Lucas Yepez <lucasyepez99@gmail.com>
2024-10-26 14:46:10 -0500

    refactored

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-4=s-y6ua&8kha!8ucdmr1yj6d*t%8n9y+-nyxanry#nmmh9573
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['messagingapp-1.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 
 # Application definition
